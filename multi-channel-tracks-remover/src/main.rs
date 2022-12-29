5d18ffa295978296b1fd65e976e94cfc6178fa53
MyBlackMIDIScore <mbms@xheonin.com>
2022-12-30 01:29:42 +0200

    First test for multi-channel-tracks-remover

--- multi-channel-tracks-remover/src/main.rs
+++ multi-channel-tracks-remover/src/main.rs
@@ -1,19 +1,55 @@
-use std::io;
-use std::io::BufRead;
+use midi_toolkit::{
+    sequence::event::Delta,
+    events::Event,
+    io::{MIDIFile, MIDIWriter},
+    pipe,
+};
 
 fn main() {
-    // Read MIDI path
-    let input = io::stdin().lock().lines().next().unwrap().unwrap();
-    let mut output = input.clone();
-    output.push_str(".chrm.mid");
+    let file = MIDIFile::open("/home/jim/Temp/test.mid", None).unwrap();
 
-    // Load the MIDI file from the given path
-    let midi = MIDIFile::open_in_ram(input, None).unwrap();
-    let writer = MIDIWriter::new(output, midi.ppq()).unwrap();
+    let writer = MIDIWriter::new("./out.mid", file.ppq()).unwrap();
 
-    // Collect events
-    let converted = file.iter_all_tracks();
-    let merged = pipe!(converted|>to_vec()|>merge_events_array()|>unwrap_items());
+    let tracks = file.iter_all_tracks();
 
-    println!("{}||{}", input, output);
+    for (n, track) in tracks.enumerate() {
+        if n < 2 {
+            continue;
+        }
+        let n = n - 2;
+        let mut track_writers = Vec::new();
+        for _ in 0..16 {
+            track_writers.push(writer.open_next_track());
+        }
+
+        let track_ch = match n {
+            0..=15 => n as u8,
+            _ => (n % 16) as u8,
+        };
+
+        let mut extra_delta = 0;
+
+        for event in track {
+            let event = event.unwrap();
+            let delta = event.delta;
+            match event.event {
+                Event::NoteOn(mut e) => {
+                    let ch = e.channel as usize;
+                    e.channel = track_ch;
+                    let ev = Delta::new(delta + extra_delta, e);
+                    track_writers[ch].write_event(ev).unwrap();
+                }
+                Event::NoteOff(mut e) => {
+                    let ch = e.channel as usize;
+                    e.channel = track_ch;
+                    let ev = Delta::new(delta, e);
+                    track_writers[ch].write_event(ev).unwrap();
+                }
+                _ => {
+                    track_writers[0].write_event(event).unwrap();
+                },
+            }
+            extra_delta += delta;
+        }
+    }
 }
